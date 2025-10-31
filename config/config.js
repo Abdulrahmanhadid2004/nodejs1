3cb649c6b204813c7a05a47c743d732cd9551018
Abdulrahman Hadid <abdulrahmanhadid04@gmail.com>
2025-10-31 16:07:58 +0300

    Describe what you changed

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || 'abdulrahmanhadid2004',
-  sessionSecret: process.env.SESSION_SECRET || 'abdulrahmanhadid2004',
+  jwtSecret: process.env.JWT_SECRET || 'a*******************',
+  sessionSecret: process.env.SESSION_SECRET || 'a*******************',
   nodeEnv: process.env.NODE_ENV || 'development'
 };
